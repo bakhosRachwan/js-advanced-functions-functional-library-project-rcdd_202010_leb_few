1b5267915f094c2edc7f6de56b993e095c17fce8
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2020-12-30 17:00:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,12 +4,23 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collections, callback) {
+      const collection = (collections instanceof Array)? collections.slice() : Object.values(collections)
+      for(let i=0; i< collection.length; i++){
+        callback(collection[i])
+      }
+      return collections
     },
 
-    map: function() {
-
+    map: function(collections, callback) {
+      const collection = (collections instanceof Array)? collections.slice() : Object.values(collections)
+      arr = [];
+      for(let i=0; i<collection.length;i++){
+        callback(collection[i]){
+           arr.pushy(collection[i])
+        }
+      }
+      return arr
     },
 
     reduce: function() {
